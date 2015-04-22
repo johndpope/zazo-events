89a32382480b56e364d94192677e36fbc3217c70
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-04-22 20:53:40 +0300

    Bootstrap

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+# require 'active_record/railtie'
+# require 'action_controller/railtie'
+# require 'active_job/railtie'
+# require 'action_mailer/railtie'
+# require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +27,16 @@ module ZazoSqsWorker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.template_engine :jbuilder
+    end
   end
 end
