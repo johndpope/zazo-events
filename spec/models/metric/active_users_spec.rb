04c81f44e80f1f99d5391321c750b3240d489c88
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-05-18 23:38:09 +0300

    Specs for `Metric::ActiveUsers` by week and month

--- spec/models/metric/active_users_spec.rb
+++ spec/models/metric/active_users_spec.rb
@@ -95,5 +95,181 @@ RSpec.describe Metric::ActiveUsers, type: :model do
                           1.days.ago.midnight => 4)
       end
     end
+
+    context 'by week' do
+      let(:group_by) { :week }
+
+      before do
+        Timecop.travel(3.weeks.ago) do
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+        end
+        Timecop.travel(2.weeks.ago) do
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+          receive_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+        end
+        Timecop.travel(1.weeks.ago) do
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+          receive_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+        end
+      end
+
+      specify do
+        is_expected.to eq(3.weeks.ago.beginning_of_week => 2,
+                          2.weeks.ago.beginning_of_week => 3,
+                          1.weeks.ago.beginning_of_week => 4)
+      end
+    end
+
+    context 'by month' do
+      let(:group_by) { :month }
+
+      before do
+        Timecop.travel(3.months.ago) do
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+        end
+        Timecop.travel(2.months.ago) do
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+          receive_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+        end
+        Timecop.travel(1.months.ago) do
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          send_video(event_data(sender_id, receiver_id, video_id))
+          receive_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+
+          sender_id = gen_user_id
+          receiver_id = gen_user_id
+          video_id = gen_video_id
+          receive_video(event_data(sender_id, receiver_id, video_id))
+          download_video(event_data(sender_id, receiver_id, video_id))
+          view_video(event_data(sender_id, receiver_id, video_id))
+        end
+      end
+
+      specify do
+        is_expected.to eq(3.months.ago.beginning_of_month => 2,
+                          2.months.ago.beginning_of_month => 3,
+                          1.months.ago.beginning_of_month => 4)
+      end
+    end
   end
 end
