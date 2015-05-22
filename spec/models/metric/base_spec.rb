6ea94d05a9665ac2ef68cdc6603103d278712fc0
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-05-22 22:04:49 +0300

    More specs for `Metric::Base`

--- spec/models/metric/base_spec.rb
+++ spec/models/metric/base_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Metric::Base, type: :model do
-  context 'group_by :day' do
-    let(:instance) { described_class.new(group_by: :day) }
+  describe '#group_by' do
+    let(:options) { { group_by: :day } }
+    let(:instance) { described_class.new(options) }
+    subject { instance.group_by }
 
-    describe '#group_by' do
-      subject { instance.group_by }
+    context 'empty' do
+      let(:options) { {} }
       it { is_expected.to eq(:day) }
+    end
+
+    context 'group_by :day' do
+      let(:options) { { group_by: :day } }
+      it { is_expected.to eq(:day) }
+    end
+
+    context 'group_by :week' do
+      let(:options) { { group_by: :week } }
+      it { is_expected.to eq(:week) }
     end
   end
 end
