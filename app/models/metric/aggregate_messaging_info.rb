43f8cbe4f674d9f6d75a9534817111d9da468867
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-05-28 22:18:39 +0300

    Improved calculation `Metric::AggregateMessagingInfo`

--- app/models/metric/aggregate_messaging_info.rb
+++ app/models/metric/aggregate_messaging_info.rb
@@ -6,17 +6,21 @@ class Metric::AggregateMessagingInfo < Metric::Base
   def generate
      %i(outgoing incoming).reduce({}) do |result, scope_name|
        scope = Event.send(scope_name, user_id)
-       total_sent = scope.by_name(%w(video s3 uploaded)).count
-       total_received = scope.by_name(%w(video kvstore received)).count
+       total_sent = reduce(scope.by_name(%w(video s3 uploaded)))
+       total_received = reduce(scope.by_name(%w(video kvstore received)))
        result[scope_name] ||= {}
        result[scope_name][:total_sent] = total_sent
        result[scope_name][:total_received] = total_received
-       result[scope_name][:undelivered_percent] = (total_sent - total_received) * 100.0 / total_sent
+       result[scope_name][:undelivered_percent] = total_sent.nonzero? && (total_sent - total_received) * 100.0 / total_sent
        result
      end
   end
 
   protected
+
+  def reduce(scope)
+    scope.distinct("data->>'video_filename'").count
+  end
 
   def set_user_id
     @user_id = attributes['user_id']
