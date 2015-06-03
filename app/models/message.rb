46cd3354529ba271791f781b1b03dacc1154ce00
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-06-03 16:23:34 +0300

    Message: added `.find`, `.all`, `.by_connection`, `#delivered?`, `#undelivered?`

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,45 @@
 class Message
+  DELIVERED_STATUSES = %i(downloaded viewed).freeze
   attr_reader :event
 
-  # +event+ must be video:s3:uploaded from S3
+  def self.all_events
+    Event.by_name(%w(video s3 uploaded)).order(:triggered_at)
+  end
+
+  def self.by_connection_events(sender_id, receiver_id)
+    all_events.with_sender(sender_id).with_receiver(receiver_id)
+  end
+
+  def self.all
+    all_events.map { |e| new(e) }
+  end
+
+  def self.by_connection(sender_id, receiver_id)
+    by_connection_events(sender_id, receiver_id).map { |e| new(e) }
+  end
+
+  def self.find(filename)
+    events = Event.by_name(%w(video s3 uploaded)).with_video_filename(filename)
+    !events.empty? && new(events.first)
+  end
+
+  # @param event [Event] - video:s3:uploaded from S3
   def initialize(event)
     @event = event
+  end
+
+  def ==(message)
+    message.is_a?(self.class) && filename == message.filename
+  end
+
+  def to_hash
+    { sender_id: data.sender_id,
+      receiver_id: data.receiver_id,
+      filename: filename,
+      date: date,
+      size: size,
+      status: status,
+      delivered: delivered? }
   end
 
   def data
@@ -26,20 +62,19 @@ class Message
     raw_params.s3.object['size']
   end
 
-  def to_hash
-    { sender_id: data.sender_id,
-      receiver_id: data.receiver_id,
-      filename: filename,
-      date: date,
-      size: size,
-      status: status }
+  def events
+    Event.with_video_filename(filename).order(:triggered_at)
   end
 
   def status
     events.last.name.last.to_sym
   end
 
-  def events
-    Event.with_video_filename(filename).order(:triggered_at)
+  def delivered?
+    DELIVERED_STATUSES.include?(status)
+  end
+
+  def undelivered?
+    !delivered?
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   let(:s3_event) { json_fixture('s3_event')['Records'] }
   let(:event) { Event.create_from_s3_event(s3_event).first }
-
   let(:instance) { described_class.new(event) }
+  let(:sender_id) { event.data['sender_id'] }
+  let(:receiver_id) { event.data['receiver_id'] }
+  let(:filename) { event.data['video_filename'] }
 
   context '#event' do
     subject { instance.event }
@@ -15,9 +17,9 @@ RSpec.describe Message, type: :model do
     subject { instance.data }
     specify do
       is_expected.to eq(
-        'sender_id' => 'RxDrzAIuF9mFw7Xx9NSM',
-        'receiver_id' => '6pqpuUZFp1zCXLykfTIx',
-        'video_filename' => 'RxDrzAIuF9mFw7Xx9NSM-6pqpuUZFp1zCXLykfTIx-98dba07c0113cc717d9fc5e5809bc998')
+        'sender_id' => sender_id,
+        'receiver_id' => receiver_id,
+        'video_filename' => filename)
     end
   end
 
@@ -28,7 +30,7 @@ RSpec.describe Message, type: :model do
 
   context '#filename' do
     subject { instance.filename }
-    it { is_expected.to eq('RxDrzAIuF9mFw7Xx9NSM-6pqpuUZFp1zCXLykfTIx-98dba07c0113cc717d9fc5e5809bc998') }
+    it { is_expected.to eq(filename) }
   end
 
   context '#date' do
@@ -44,24 +46,26 @@ RSpec.describe Message, type: :model do
   context 'to_hash' do
     subject { instance.to_hash }
     specify do
-      is_expected.to eq(sender_id: 'RxDrzAIuF9mFw7Xx9NSM',
-                        receiver_id: '6pqpuUZFp1zCXLykfTIx',
-                        filename: 'RxDrzAIuF9mFw7Xx9NSM-6pqpuUZFp1zCXLykfTIx-98dba07c0113cc717d9fc5e5809bc998',
+      is_expected.to eq(sender_id: sender_id,
+                        receiver_id: receiver_id,
+                        filename: filename,
                         date: '2015-04-22T18:01:20.663Z'.to_datetime,
                         size: 94_555,
-                        status: :uploaded)
+                        status: :uploaded,
+                        delivered: false)
     end
   end
 
   context 'to_json' do
     subject { instance.to_json }
     specify do
-      is_expected.to eq({ sender_id: 'RxDrzAIuF9mFw7Xx9NSM',
-                          receiver_id: '6pqpuUZFp1zCXLykfTIx',
-                          filename: 'RxDrzAIuF9mFw7Xx9NSM-6pqpuUZFp1zCXLykfTIx-98dba07c0113cc717d9fc5e5809bc998',
+      is_expected.to eq({ sender_id: sender_id,
+                          receiver_id: receiver_id,
+                          filename: filename,
                           date: '2015-04-22T18:01:20.663Z',
                           size: 94_555,
-                          status: :uploaded}.to_json)
+                          status: :uploaded,
+                          delivered: false}.to_json)
     end
   end
 
@@ -102,5 +106,93 @@ RSpec.describe Message, type: :model do
 
       it { is_expected.to eq(:viewed) }
     end
+  end
+
+  describe '#delivered?' do
+    subject { instance.delivered? }
+
+    context 'uploaded' do
+      it { is_expected.to be_falsey }
+    end
+
+    context 'received' do
+      before do
+        receive_video instance.data
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'downloaded' do
+      before do
+        receive_video instance.data
+        download_video instance.data
+      end
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'viewed' do
+      before do
+        receive_video instance.data
+        download_video instance.data
+        view_video instance.data
+      end
+
+      it { is_expected.to be_truthy }
+    end
+  end
+
+  describe '#undelivered?' do
+    subject { instance.undelivered? }
+
+    context 'uploaded' do
+      it { is_expected.to be_truthy }
+    end
+
+    context 'received' do
+      before do
+        receive_video instance.data
+      end
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'downloaded' do
+      before do
+        receive_video instance.data
+        download_video instance.data
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'viewed' do
+      before do
+        receive_video instance.data
+        download_video instance.data
+        view_video instance.data
+      end
+
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe '.all' do
+    before { event.save }
+    subject { described_class.all }
+    it { is_expected.to eq([instance]) }
+  end
+
+  describe '.by_connection' do
+    before { event.save }
+    subject { described_class.by_connection(sender_id, receiver_id) }
+    it { is_expected.to eq([instance]) }
+  end
+
+  describe '.find' do
+    before { event.save }
+    subject { described_class.find(filename) }
+    it { is_expected.to eq(instance) }
   end
 end
