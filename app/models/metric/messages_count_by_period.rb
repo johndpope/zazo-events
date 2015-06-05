c7c6b2df7615d7c7f90c36f1540b2754928a10b3
Ivan Kornilov <vano468@gmail.com>
2015-06-05 19:53:26 +0300

    Implement MessagesCountByPeriod metric

---
+++ app/models/metric/messages_count_by_period.rb
@@ -0,0 +1,28 @@
+class Metric::MessagesCountByPeriod < Metric::Base
+  include Metric::GroupableByTimeFrame
+  after_initialize :set_attributes
+
+  attr_accessor :user_id, :group_by, :since
+
+  validates :user_id, presence: true
+  # todo: since date validation
+
+  def generate
+    scope = Event.send :with_sender, user_id
+    reduce(scope.by_name(%w(video s3 uploaded))).sort.to_h
+  end
+
+protected
+
+  def reduce(scope)
+    scope = scope.since since if since.present?
+    scope.group("DATE_TRUNC('#{group_by}', triggered_at)")
+         .distinct("data->>'video_filename'")
+         .count("data->>'video_filename'")
+  end
+
+  def set_attributes
+    @user_id   = attributes['user_id']
+    @since     = attributes['since']
+  end
+end
