cbdf492886e430f1cb5c6f9dc333b45c1c64439c
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-06-08 22:07:45 +0300

    Fix error and code style

--- app/models/metric/messages_count_by_period.rb
+++ app/models/metric/messages_count_by_period.rb
@@ -5,20 +5,20 @@ class Metric::MessagesCountByPeriod < Metric::Base
   attr_accessor :user_id, :group_by, :since
 
   validates :user_id, presence: true
-  # todo: since date validation
+  # TODO: since date validation
 
   def generate
-    scope = Event.with_sender, user_id
+    scope = Event.with_sender user_id
     reduce(scope.by_name(%w(video s3 uploaded))).sort.to_h
   end
 
-protected
+  protected
 
   def reduce(scope)
     scope = scope.since since if since.present?
     scope.group("DATE_TRUNC('#{group_by}', triggered_at)")
-         .distinct("data->>'video_filename'")
-         .count("data->>'video_filename'")
+      .distinct("data->>'video_filename'")
+      .count("data->>'video_filename'")
   end
 
   def set_attributes
