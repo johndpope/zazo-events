51a3f0ba85b5a8526a801d3cfa88cea04fdd4327
Ivan Kornilov <vano468@gmail.com>
2015-06-12 18:02:19 +0300

    Fix MessagesCountByPeriod metric: use since date

--- app/models/metric/messages_count_by_period.rb
+++ app/models/metric/messages_count_by_period.rb
@@ -22,12 +22,13 @@ class Metric::MessagesCountByPeriod < Metric::Base
         COUNT(DISTINCT data->>'video_filename')
       FROM events
       WHERE data->>'sender_id' IN (?)
+      AND triggered_at > ?
       AND name = ARRAY[?]::varchar[]
       GROUP BY period, sender
       ORDER BY sender, period
     SQL
     sql = Event.send :sanitize_sql_array,
-                     [sql, group_by, users_ids, events]
+                     [sql, group_by, users_ids, since, events]
     Event.connection.select_all sql
   end
 
