105d51a3298521d4775b3b4b4d2e7a72771bcd0f
Ivan Kornilov <vano468@gmail.com>
2015-09-01 16:33:38 +0300

    Refactor ActiveUsers metric with RAW SQL

--- app/models/metric/active_users.rb
+++ app/models/metric/active_users.rb
@@ -2,29 +2,37 @@ class Metric::ActiveUsers < Metric::Base
   include Metric::GroupableByTimeFrame
 
   def generate
-    zip(reduce_by_users(video_uploaded), reduce_by_users(video_viewed))
+    results.each_with_object({}) do |row, memo|
+      memo[row['period']] = row['count']
+    end
   end
 
   private
 
-  def video_uploaded
-    @video_uploaded ||= common_scope.video_s3_uploaded.group("data->>'sender_id'").count("data->>'sender_id'")
-  end
-
-  def video_viewed
-    @video_viewed ||= common_scope.by_name(%w(video kvstore viewed)).group("data->>'receiver_id'").count("data->>'receiver_id'")
-  end
-
-  def common_scope
-    Event.send(:"group_by_#{group_by}", :triggered_at)
-  end
-
-  def zip(first, second)
-    first.each_with_object({}) do |(time_frame, user_ids), memo|
-      total = Set.new
-      total += Set.new(user_ids) if user_ids.present?
-      total += Set.new(second[time_frame]) if second[time_frame].present?
-      memo[time_frame] = total.size
-    end
+  def results
+    run_raw_query <<-SQL
+      WITH viewed AS (
+        SELECT
+          triggered_at,
+          data->>'receiver_id' user_mkey
+        FROM events
+        WHERE name = ARRAY['video', 'kvstore', 'viewed']::VARCHAR[]
+      ), uploaded AS (
+        SELECT
+          triggered_at,
+          data->>'sender_id' user_mkey
+        FROM events
+        WHERE name = ARRAY['video', 's3', 'uploaded']::VARCHAR[]
+      ), merged AS (
+        SELECT *
+        FROM viewed
+        UNION ALL (SELECT * FROM uploaded)
+      ) SELECT
+          DATE_TRUNC('#{group_by}', triggered_at) period,
+          COUNT(DISTINCT user_mkey)
+        FROM merged
+        GROUP BY DATE_TRUNC('#{group_by}', triggered_at)
+        ORDER BY period
+    SQL
   end
 end
