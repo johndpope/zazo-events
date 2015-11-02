41b8a2bc0c6617685bc927c72e1fee11fd9ea03f
Ivan Kornilov <vano468@gmail.com>
2015-11-02 15:00:37 +0300

    Fix n+1 query problem for messages failures metric for autonotification

--- app/models/metric/messages_failures_autonotification.rb
+++ app/models/metric/messages_failures_autonotification.rb
@@ -1,8 +1,8 @@
 class Metric::MessagesFailuresAutonotification < Metric::MessagesFailures
   def data
     messages.each_with_object(aggregated_by_platforms.except(:unknown_to_unknown)) do |message, result|
-      if message.data.client_platform == 'android' && message.data.client_version.to_i >= 112
-        direction = :"#{message.data.client_platform}_to_#{message.receiver_platform}"
+      if message.client_platform == :android && message.client_version >= 112
+        direction = :"#{message.client_platform}_to_#{message.receiver_platform}"
         result[direction] = handle_data_by_message result.fetch(direction, sample), message
       end
     end
