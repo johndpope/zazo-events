99718649ed71c76b230496fd6a9a2b74fed87b2c
Ivan Kornilov <vano468@gmail.com>
2015-12-14 19:35:07 +0300

    Change ActiveDaysByActiveFriends metric to ActiveDaysAndMessagesByActiveFriends

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Metric, type: :model do
   describe '.all' do
     subject { described_class.all }
     let(:all_metrics) do
-      [Metric::ActiveDaysByActiveFriends,
+      [Metric::ActiveDaysAndMessagesByActiveFriends,
        Metric::ActiveUsers,
        Metric::AggregateMessagingInfo,
        Metric::Filter::NonMarketing,
