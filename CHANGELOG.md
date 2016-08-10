b6e7f163b1bf0f6726dd605ad086301c259ea283
Ivan Kornilov <vano468@gmail.com>
2016-08-10 13:23:52 +0300

    Change status routes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ### 1.15.5-wip.1
 - :arrow_up: Removed all legacy code
+- :arrow_up: Changed status routes
 
 ### 1.15.5
 - :bulb: Implemented `InvitationConversionData` metric
--- app/controllers/api/v1/status_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::StatusController < ApplicationController
-  def index
-    render json: { app_name: Settings.app_name, version: Settings.version }
-  end
-
-  def heartbeat
-    render text: 'OK'
-  end
-end
